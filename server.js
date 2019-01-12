9dabf69101bc1d8b0b6df603789a8ed83a2a7502
黃政瑋 <aqwefghnm@gmail.com>
2019-01-12 14:39:16 +0800

    Add ssl

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const port = 11122;
 
@@ -9,6 +11,17 @@ app.get('/get_data', function(req, res) {
 });
 
 
-app.listen(port, function(){
-	console.log('Listening Port');
+const key = fs.readFileSync('/home/wp2018/ssl/private.key');
+const cert = fs.readFileSync('/home/wp2018/ssl/certificate.crt');
+
+var opt = {
+  key: key,
+  cert: cert,
+};
+
+https.createServer(opt, app).listen(port, function(){
+  console.log('---Connected---');
 });
+//app.listen(port, function(){
+//	console.log('Listening Port');
+//});
